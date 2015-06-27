706e5448e3103819be832d9723887abf88f2bfb4
Philip Morkisch <philip.morkisch@brandung.de>
2015-06-27 21:50:02 +0200

    implemented new grunt tasks, updated node modules, commented out tasks that need to be refactored

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,9 @@ module.exports = function (grunt) {
 		path = require('path'),
 		target = grunt.option('target'),
 		cwd = path.resolve(process.cwd(), ''),
-		dbug = !!grunt.option('dbug');
+		//dbug = !!grunt.option('dbug'),
+		component = grunt.option('name') || '',
+		user = process.env['USERPROFILE'].split(path.sep)[2];
 
 	// measures the time grunt takes to complete all tasks
 	// https://www.npmjs.org/package/time-grunt
@@ -11,14 +13,11 @@ module.exports = function (grunt) {
 
 	// load only the modules that are currently needed instead of loading all modules on every build
 	// https://www.npmjs.org/package/jit-grunt
+	// TODO refactor
 	require('jit-grunt')(grunt, {
 		'code:compress': [
 			'uglify',
 			'cssmin'
-		],
-		'code:validate': [
-			'jslint',
-			'csslint'
 		],
 		'project:init': [
 			'mkdir:project',
@@ -35,7 +34,7 @@ module.exports = function (grunt) {
 		],
 		'project:sync': [
 			'browserSync',
-			'dbug',
+			//'dbug',
 			'concat',
 			'watch'
 		],
@@ -136,6 +135,7 @@ module.exports = function (grunt) {
 
 		// append assets into specific files
 		// @brandung
+		// TODO refactor
 		appendAssets: {
 			html: {
 				startBlock: '<!-- start|bra-pb: html -->\n',
@@ -161,10 +161,82 @@ module.exports = function (grunt) {
 			}
 		},
 
+		// creates files
+		// https://www.npmjs.com/package/grunt-file-creator
+		'file-creator': {
+			files: [
+				{
+					file: '<%= pkg.private %>/sass/component/' + component + '.scss',
+					method: function (fs, fd, done) {
+						var date = new Date(),
+							content = "@charset \"utf-8\";\n" +
+								"/**\n" +
+								" * brandung " + component + " v1.0.0\n" +
+								" *\n" +
+								" * Copyright brandung GmbH & Co.KG\n" +
+								" * http://www.brandung.de/\n" +
+								" *\n" +
+								" * Date: " + date.toISOString().substring(0, 10) + "\n" +
+								" * MIT License (MIT)\n" +
+								" */\n" +
+								"\n" +
+								"@import '../partials/functions';\n" +
+								"@import '../partials/variables';\n" +
+								"@import '../partials/mixins';\n" +
+								"\n" +
+								"." + component + " {\n" +
+								"\t\n" +
+								"}\n";
+						fs.writeSync(fd, content);
+
+						done();
+					}
+				},
+				{
+					file: '<%= pkg.private %>/templates/tpl/partials/component/' + component + '.tpl',
+					method: function (fs, fd, done) {
+						var date = new Date();
+						content = "<!--\n" +
+							" brandung " + component + " v1.0.0\n" +
+							"\n" +
+							" Copyright brandung GmbH & Co.KG\n" +
+							" http://www.brandung.de/\n" +
+							"\n" +
+							" Date: " + date.toISOString().substring(0, 10) + "\n" +
+							" MIT License (MIT)\n" +
+							" -->\n";
+						fs.writeSync(fd, content);
+
+						done();
+					}
+				}
+			],
+			views: [
+				{
+					file: '<%= pkg.private %>/templates/tpl/partials/views/' + component + '.tpl',
+					method: function (fs, fd, done) {
+						var date = new Date();
+						content = "<!--\n" +
+							" brandung " + component + " v1.0.0\n" +
+							"\n" +
+							" Copyright brandung GmbH & Co.KG\n" +
+							" http://www.brandung.de/\n" +
+							"\n" +
+							" Date: " + date.toISOString().substring(0, 10) + "\n" +
+							" MIT License (MIT)\n" +
+							" -->\n";
+						fs.writeSync(fd, content);
+
+						done();
+					}
+				}
+			]
+		},
+
 		// replaces a placeholder for the assets path relative to the project type
 		// https://npmjs.org/package/grunt-text-replace
 		replace: {
-			appendAssetsHTML: {
+			/*appendAssetsHTML: {
 				src: ['<%= pkg.private %>/templates/tpl/_modules.tpl'],
 				overwrite: true,
 				replacements: [
@@ -193,13 +265,13 @@ module.exports = function (grunt) {
 						to: '<%= pkg.tempAssets %>'
 					}
 				]
-			},
+			},*/
 			project: {
 				src: [
 					'<%= pkg.private %>/templates/tpl/**/*.tpl',
 					'<%= pkg.private %>/js/global.js',
-					'<%= pkg.private %>/js/dbug/dbug.js',
-					'<%= pkg.private %>/sass/partials/*.*',
+					//'<%= pkg.private %>/js/dbug/dbug.js',
+					'<%= pkg.private %>/sass/**/*.*',
 					'bower.json'
 				],
 				overwrite: true,
@@ -234,20 +306,101 @@ module.exports = function (grunt) {
 					}
 				]
 			}
+			unique: {
+				src: [
+					'<%= pkg.public %>/js/main.js'
+				],
+				overwrite: true,
+				replacements: [
+					{
+						from: /<@unique@>/g,
+						to: (new Date().getTime() + new Date().getTime())
+					}
+				]
+			},
+			deleteBlockCSS: {
+				src: [
+					'<%= pkg.public %>/css/*.css',
+					'<%= pkg.public %>/css/component/*.css'
+				],
+				overwrite: true,
+				replacements: [
+					{
+						from: /\/\*\s?<@delete*(\S*)(\n|\r|.)*?\s?delete@>\s?\*\//igm,
+						to: ''
+					}
+				]
+			},
+			deleteBlockJS: {
+				src: [
+					'<%= pkg.public %>/js/*.js',
+					'<%= pkg.public %>/js/plugins/*.js',
+					'<%= pkg.public %>/js/helpers/*.js',
+					'<%= pkg.public %>/js/functions/*.js'
+				],
+				overwrite: true,
+				replacements: [
+					{
+						from: /\/\/\s?<@delete*(\S*)(\n|\r|.)*?\/\/\s?delete@>/igm,
+						to: ''
+					}
+				]
+			},
+			module: {
+				src: [
+					'<%= pkg.private %>/js/global.js'
+				],
+				overwrite: true,
+				replacements: [
+					{
+						from: /\/\/\s?<@newComponent@>/ig,
+						to: ",\n\t\t\t\t\t\t{\n\t\t\t\t\t\t\tcondition: $('." + component + "'),\n\t\t\t\t\t\t\tfetch: [\n\t\t\t\t\t\t\t\tBrandung.GlobalVars.folderPath + 'css/component/" + component + ".css'\n\t\t\t\t\t\t\t],\n\t\t\t\t\t\t\tunique: Brandung.Util.getUnique()\n\t\t\t\t\t\t}// <@newComponent@>"
+					}
+				]
+			},
+			swigModule: {
+				src: [
+					'<%= pkg.private %>/templates/tpl/_modules.tpl'
+				],
+				overwrite: true,
+				replacements: [
+					{
+						from: /<!--\s?<@newComponent@>\s?-->/ig,
+						to: "<h3 class=\"mod-headline\">" +
+						component.charAt(0).toUpperCase() + component.slice(1) +
+						"</h3>\n" +
+						"\t\t\t\t{% include \"./partials/component/" + component.toString() + ".tpl\" %} \n\n" +
+						"\t\t\t\t<!-- <@newComponent@> -->"
+					}
+				]
+			},
+			swigView: {
+				src: [
+					'<%= pkg.private %>/templates/tpl/_modules.tpl'
+				],
+				overwrite: true,
+				replacements: [
+					{
+						from: /<!--\s?<@newView@>\s?-->/ig,
+						to: "<h3 class=\"mod-headline\">" +
+						component.charAt(0).toUpperCase() + component.slice(1) +
+						"</h3>\n" +
+						"\t\t\t\t{% include \"./partials/views/" + component.toString() + ".tpl\" %} \n\n" +
+						"\t\t\t\t<!-- <@newView@> -->"
+					}
+				]
+			}
 		},
 
 		// compiles sass files using libsass (damn fast!)
 		// https://www.npmjs.org/package/grunt-sass
 		sass: {
-			options: {
-				sourceMap: true,
-				sourceComments: 'map'
-			},
 			files: {
-				files: {
-					'<%= pkg.public %>/css/all-old-ie.css': '<%= pkg.private %>/sass/all-old-ie.scss',
-					'<%= pkg.public %>/css/main.css': '<%= pkg.private %>/sass/main.scss'
-				}
+				expand: true,
+				cwd: '<%= pkg.private %>/sass',
+				src: ['**/*.scss'],
+				dest: '<%= pkg.public %>/css',
+				ext: '.css'
 			}
 		},
 
@@ -256,7 +409,7 @@ module.exports = function (grunt) {
 		concat: {
 			dist: {
 				src: [
-					'<%= pkg.public %>/js/libs/vendor/basket/basket.full.min.js',
+					'<%= pkg.public %>/js/libs/vendor/basket/basket.full.custom.min.js',
 					'<%= pkg.private %>/js/global.js'
 				],
 				dest: '<%= pkg.public %>/js/main.js'
@@ -272,21 +425,42 @@ module.exports = function (grunt) {
 				src: '**',
 				dest: '<%= pkg.public %>/js/libs/'
 			},
-			widgets: {
+			plugin: {
 				expand: true,
-				cwd: '<%= pkg.private %>/js/widgets/',
+				cwd: '<%= pkg.private %>/js/plugin/',
 				src: '*',
-				dest: '<%= pkg.public %>/js/widgets/',
+				dest: '<%= pkg.public %>/js/plugin/',
 				flatten: true
 			},
-			scssmod: {
+			handler: {
+				expand: true,
+				cwd: '<%= pkg.private %>/js/handler/',
+				src: '*',
+				dest: '<%= pkg.public %>/js/handler/',
+				flatten: true
+			},
+			'function': {
+				expand: true,
+				cwd: '<%= pkg.private %>/js/function/',
+				src: '*',
+				dest: '<%= pkg.public %>/js/function/',
+				flatten: true
+			},
+			component: {
+				expand: true,
+				cwd: '<%= pkg.private %>/js/component/',
+				src: '*',
+				dest: '<%= pkg.public %>/js/component/',
+				flatten: true
+			},
+			/*scssmod: {
 				expand: true,
 				cwd: '<%= pkg.private %>/sass/mod/',
 				src: '**',
 				dest: '<%= pkg.private %>/sass/partials/mod/',
 				flatten: true,
 				filter: 'isFile'
-			},
+			},*/
 			rootFiles: {
 				expand: true,
 				cwd: '<%= pkg.private %>/',
@@ -383,45 +557,6 @@ module.exports = function (grunt) {
 			}
 		},
 
-		// validates js and saves all found errors and warnings in a log file
-		// https://npmjs.org/package/grunt-jslint
-		jslint: {
-			client: {
-				src: [
-					'<%= pkg.private %>/js/*.js',
-					'<%= pkg.private %>/js/helpers/*.js',
-					'<%= pkg.private %>/js/widgets/*.js',
-					'<%= pkg.private %>/js/ngapp/**/*.js'
-				],
-				directives: {
-					browser: true,
-					predef: [
-						'jQuery',
-						'Modernizr',
-						'angular'
-					]
-				},
-				options: {
-					log: '<%= pkg.private %>/js/jslint.log',
-					failOnError: false,
-					errorsOnly: true
-				}
-			}
-		},
-
-		// validates css and checks for possible optimizations
-		// https://npmjs.org/package/grunt-contrib-csslint
-		csslint: {
-			strict: {
-				options: {
-					import: 2
-				},
-				src: [
-					'<%= pkg.public %>/css/*.css'
-				]
-			}
-		},
-
 		// do the bower packaging stuff
 		// https://www.npmjs.org/package/grunt-bower-task
 		bower: {
@@ -465,14 +600,17 @@ module.exports = function (grunt) {
 			styles: {
 				files: '<%= pkg.private %>/sass/**/*.scss',
 				tasks: [
-					'sass'
+					'sass',
+					'sassToHtml'
 				]
 			},
 			scripts: {
 				files: '<%= pkg.private %>/js/**/*.js',
 				tasks: [
-					'copy:libs',
-					'copy:widgets',
+					'copy:plugin',
+					'copy:handler',
+					'copy:function',
+					'copy:component',
 					'concat',
 					'clean:js'
 				]
@@ -516,8 +654,9 @@ module.exports = function (grunt) {
 	 * MultiTasks
 	 */
 
-		// appendAssets task
-		// task for insert assets into file
+	// appendAssets task
+	// task for insert assets into file
+	// TODO refactor
 	grunt.registerMultiTask('appendAssets', 'Insert JS/SCSS/HTML assets to a file', function () {
 		// get all files in target folder
 		var paths = grunt.file.expand(this.data.paths),
@@ -572,7 +711,7 @@ module.exports = function (grunt) {
 	});
 
 	// dbug task
-	grunt.registerTask('dbug', 'Activate debug tools', function (n) {
+	/*grunt.registerTask('dbug', 'Activate debug tools', function (n) {
 		console.log("\nDEBUG MODE: " + dbug);
 		// define placeholder
 		var startBlock = '// --- start|bra-pb: js ---\n',
@@ -591,8 +730,89 @@ module.exports = function (grunt) {
 		}
 		// start task
 		grunt.task.run('replace:appendAssetsJS');
-	});
+	});*/
 
+	grunt.registerTask('sassToHtml', 'Creating markup out of sass variables', function (n) {
+		var variablesFile = grunt.file.read(pkg.private + '/sass/partials/_variables.scss'),
+			getMarkup = function (type) {
+				var typeObj = {
+						colors: {
+							blockRegex: /\/\*\s?<@colors*(\S*)(\n|\r|.)*?\s?colors@>\s?\*\//igm,
+							mapItemRegex: /\'([a-z0-9]-*)+\':\s?#[a-fA-F0-9]{3,6}/g,
+							html: function (key, value) {
+								return "\t<div class='col-xs-2'>\n" +
+									"\t\t<div style='background: " + value + "; padding: 15px; width: 100%; border: solid 1px black; height: 100px;'>" +
+									"\t\t</div>\n" +
+									"\t\t<b>" + key + ":</b> " + value + "\n" +
+									"\t</div>\n";
+							}
+						},
+						icons: {
+							blockRegex: /\/\*\s?<@icons*(\S*)(\n|\r|.)*?\s?icons@>\s?\*\//igm,
+							mapItemRegex: /([a-zA-Z0-9]-*)+:\s?('|")\\[a-z0-9]+('|")/g,
+							html: function (key, value) {
+								return "\t<div class='col-xs-2'>\n" +
+									"\t\t<div class='icon-before icon-" + key + "' style='text-align:center; font-family: \"icomoon\"; padding: 15px; width: 100%; border: solid 1px black; height: 75px; color: black; line-height:40px; font-size:40px;'>" +
+									"\t\t</div>\n" +
+									"\t\t<b>.icon-" + key + ":</b> " + value + "\n" +
+									"\t</div>\n";
+							}
+						},
+						breakpoints: {
+							blockRegex: /\/\*\s?<@breakpoints*(\S*)(\n|\r|.)*?\s?breakpoints@>\s?\*\//igm,
+							mapItemRegex: /\$([a-zA-Z0-9]-*)+:\s?[0-9]{3,4}/g,
+							html: function (key, value) {
+								key = key.toUpperCase();
+								key = key.replace('$', '');
+
+								return "<div style='width: " + value + "px; background: black; box-sizing: border-box; padding: 0 15px; height:40px; line-height: 40px; color:white; margin-bottom: 20px;'>" + key + "<span class='right'>" + value + "px</span></div>\n";
+							}
+						}
+					},
+					html = '',
+					count = 0,
+					map,
+					string;
+
+				map = variablesFile.match(typeObj[type].blockRegex)[0];
+				map = map.match(typeObj[type].mapItemRegex);
+
+				if(type === 'colors' || type === 'icons') {
+					html =  "<div class='row'>\n";
+				}
+
+				for(var i = 0, len = map.length; i < len; i += 1) {
+					string = map[i].replace(/\s/g, '');
+					string = string.split(':');
+
+					html += typeObj[type].html(string[0], string[1]);
+
+					count += 1;
+
+					if(count === 6) {
+						if(type === 'colors' || type === 'icons') {
+							html +=  "</div>\n<div class='row'>\n";
+						}
+
+						count = 0;
+					}
+				}
+
+				if(type === 'colors' || type === 'icons') {
+					html +=  "</div>";
+				}
+
+				return html;
+			};
+
+		console.log('sassToHtml: Updating colors.tpl');
+		console.log('sassToHtml: Updating icons.tpl');
+		console.log('sassToHtml: Updating breakpoints.tpl');
+
+		grunt.file.write(pkg.private + '/templates/tpl/partials/colors.tpl', getMarkup('colors'));
+		grunt.file.write(pkg.private + '/templates/tpl/partials/icons.tpl', getMarkup('icons'));
+		grunt.file.write(pkg.private + '/templates/tpl/partials/breakpoints.tpl', getMarkup('breakpoints'));
+	});
 
 	/**
 	 * dev tasks
@@ -602,19 +822,18 @@ module.exports = function (grunt) {
 		'uglify',
 		'cssmin'
 	]);
-	grunt.registerTask('code:validate', [
-		'jslint',
-		'csslint'
-	]);
 
 	// Tasks: project builder
 	grunt.registerTask('default', [
 		'sass',
 		'tasty_swig',
-		'copy:libs',
-		'copy:widgets',
+		'copy:plugin',
+		'copy:handler',
+		'copy:function',
+		'copy:component',
 		'concat',
-		'clean:js'
+		'clean:js',
+		'sassToHtml'
 	]);
 	grunt.registerTask('project:init', [
 		'mkdir:project',
@@ -630,10 +849,18 @@ module.exports = function (grunt) {
 		'project:sync'
 	]);
 	grunt.registerTask('project:sync', [
+		'default',
 		'browserSync',
-		'dbug',
+		//'dbug',
 		'concat',
 		'watch'
+	]);
+	grunt.registerTask('project:deploy', [
+		'default',
+		'replace:deleteBlockCSS',
+		'replace:unique',
+		'replace:deleteBlockJS',
+		'code:compress'
 	]);
 	grunt.registerTask('project:zip', [
 		'mkdir:pack',
@@ -646,7 +873,8 @@ module.exports = function (grunt) {
 	]);
 
 	// Tasks: download builder
-	grunt.registerTask('build:installModules', [
+	// TODO refactor
+	/*grunt.registerTask('build:installModules', [
 		'bower:install',
 		'copy:scssmod',
 		'build:insertAssets',
@@ -658,5 +886,21 @@ module.exports = function (grunt) {
 		'appendAssets:html',
 		'appendAssets:scss',
 		'appendAssets:js'
+	]);*/
+	grunt.registerTask('build:styleguide', [
+		'sassToHtml'
+	]);
+	grunt.registerTask('build:component', [
+		'file-creator:files',
+		'replace:module',
+		'replace:swigModule',
+		'default',
+		'project:sync'
+	]);
+	grunt.registerTask('build:view', [
+		'file-creator:views',
+		'replace:swigView',
+		'default',
+		'project:sync'
 	]);
 };
